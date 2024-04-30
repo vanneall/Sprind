79a655ce56c9e1b52c281692ea3a097fcc8b1d36
Ivan Demin <defoxyweex@gmail.com>
2024-04-30 19:24:43 +0300

    Сделана разметка для страницы корзины

--- app/src/main/java/ru/point/sprind/presenter/cart/CartFragment.kt
+++ app/src/main/java/ru/point/sprind/presenter/cart/CartFragment.kt
@@ -5,15 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.point.sprind.R
+import ru.point.sprind.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
+
+    private lateinit var binding: FragmentCartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        binding = FragmentCartBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
