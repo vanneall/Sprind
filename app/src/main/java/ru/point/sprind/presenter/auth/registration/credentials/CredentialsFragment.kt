d26f5850ac92cf2334bfecd3e5a9f2b0e8b0a462
Ivan Demin <defoxyweex@gmail.com>
2024-05-07 10:53:00 +0300

    Добавлен экран для ввода учетных данныхх при регистрации

--- app/src/main/java/ru/point/sprind/presenter/auth/registration/credentials/CredentialsFragment.kt
+++ app/src/main/java/ru/point/sprind/presenter/auth/registration/credentials/CredentialsFragment.kt
@@ -1,21 +1,21 @@
-package ru.point.sprind.presenter.auth.registration
+package ru.point.sprind.presenter.auth.registration.credentials
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.point.sprind.databinding.FragmentRegistrationBinding
+import ru.point.sprind.databinding.FragmentCredentialsBinding
 
-class RegistrationFragment : Fragment() {
+class CredentialsFragment : Fragment() {
 
-    private lateinit var binding: FragmentRegistrationBinding
+    private lateinit var binding: FragmentCredentialsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = FragmentRegistrationBinding.inflate(layoutInflater)
+        binding = FragmentCredentialsBinding.inflate(layoutInflater)
         return binding.root
     }
 }
